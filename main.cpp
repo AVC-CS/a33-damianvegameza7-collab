c1c8bb53f5bca004adab02e72aeef6364febce95
Damian Vega Meza <damian@LAPTOP-H44S0OTB.localdomain>
2026-03-09 14:11:07 -0700

    Assignment a33 done

--- main.cpp
+++ main.cpp
@@ -8,11 +8,12 @@ int main()
     double F;
 
     // TODO: [Step 1] Input a Celcius degree as a "double" type value
-
+    cout << "Enter the temperature in Celsius\n";
+    cin >> C;
     // TODO: [Step 2] Convert it to a Fahrenheit degree using the Formula F = (9 / 5.0)* C + 32
-
+    F = (9 / 5.0) * C + 32;
     cout << setprecision(2) << fixed;
     // TODO: [Step 3] Print the converted Fahrenheit
-
+    cout << "The temperature in Farenheit is: " << F << endl;
     return 0;
 }
